1c903cd15e993a9ddb7bc9a6f382ab3d45a0d267
Edenas Pocius <edenas.pocius@gmail.com>
2026-04-21 19:21:02 +0300

    idex puslapyja jau rodoma statistika ir pridetas base.html sablonas(dizainas)

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Car, Service, Order, OrderLine
 # Create your views here.
 
 def index(request):
-    return  render(request, template_name="index.html")
+    context = {
+        'num_cars': Car.objects.count(),
+        'num_service': Service.objects.count(),
+        'num_orders_done': Order.objects.filter(status='c').count(),
+    }
+    return  render(request, template_name="index.html", context=context)
