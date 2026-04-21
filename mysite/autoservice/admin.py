fc1bad343bc02be3f273898f74f1ffdcdfb10df8
Edenas Pocius <edenas.pocius@gmail.com>
2026-04-21 16:50:42 +0300

    admine prie orders matosi (CAR, DATE, CLIENT, STAUS) ir kad redaguojasi atliktos paslaugos

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Car, Service, Order, OrderLine
 
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['car', 'date', 'client', 'status']
+    inlines = [OrderLineInLine]
+
 # Register your models here.
-admin.site.register(Car)
 admin.site.register(Service)
-admin.site.register(Order)
+admin.site.register(Car)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine)
