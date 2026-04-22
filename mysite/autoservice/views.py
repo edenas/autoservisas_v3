5eba3d281267fd8d713dca7615b3650ed5a31425
Edenas Pocius <edenas.pocius@gmail.com>
2026-04-22 21:06:32 +0300

    apsilankymu skaicius

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -4,10 +4,13 @@ from django.views import generic
 # Create your views here.
 
 def index(request):
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_cars': Car.objects.count(),
         'num_service': Service.objects.count(),
         'num_orders_done': Order.objects.filter(status='c').count(),
+        'num_visits': num_visits,
     }
     return  render(request, template_name="index.html", context=context)
 
